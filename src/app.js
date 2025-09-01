f158ccbec90c7ce32d7611f1cdfe1ef36a37f3bc
ShivaKumar-Bale <baleshivakumar1056@gmail.com>
2025-09-01 11:14:30 +0530

    added user schema

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Virat",
+    lastName: "Kohli",
+    emailId: "virat@kohli.com",
+    age: 36,
+    password: "virat@kumar",
+  });
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (error) {
+    res.status(400).send("Can't add User" + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
